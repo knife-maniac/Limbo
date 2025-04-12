510ef1a0ad78655374f493e6f469dea040d82a9e
Ruben Quast <Ruben.Quast@outlook.fr>
2025-04-12 17:36:03 +0200

    Initial commit

---
+++ index.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import {readFile, stat, writeFile} from 'fs/promises';
+
+const app = express();
+app.use(express.static(__dirname));
+
+const port: number = 666;
+const stateFilePath = 'state.txt';
+
+app.listen(port, () => {
+    console.log('http://localhost:' + port);
+});
+
+app.get('/', (_req: any, res: any) => {
+    res.send('Hello world!');
+});
+
+app.get('/planner', (_req: any, res: any) => {
+    res.sendFile('index.html', { root: __dirname });
+});
+
+app.get('/readState', async (_req: any, res: any) => {
+    const fileExists = !!(await stat(stateFilePath).catch(e => false));
+    const state = fileExists ? (await readFile(stateFilePath)).toString() : '';
+    res.send(state);
+});
+
+app.get('/writeState/:state', async (req: any, res: any) => {
+    await writeFile(stateFilePath, req.params.state);
+    res.send('Ok');
+});
+
