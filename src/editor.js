203a08fc7c8e3f96178ac7cb3beb0d0263323595
Ruben Quast <Ruben.Quast@outlook.fr>
2025-04-26 20:26:07 +0200

    Improve task editor style

--- src/editor.js
+++ src/editor.js
@@ -22,8 +22,8 @@ function openTaskEditor(task = null, bucketId = null) {
     if (action === 'create') {
         taskEditor.setAttribute('data-bucket', bucketId);
     } else if (action === 'edit') {
-        taskEditor.querySelector('input#title').value = task.title;
-        taskEditor.querySelector('input#description').value = task.description;
+        taskEditor.querySelector('#title').value = task.title;
+        taskEditor.querySelector('#description').value = task.description;
         taskEditor.setAttribute('data-task', task.id);
     }
     taskEditor.style.display = '';
@@ -32,8 +32,8 @@ function openTaskEditor(task = null, bucketId = null) {
 
 async function saveTask() {
     const taskEditor = document.getElementById('task_editor');
-    const title = taskEditor.querySelector('input#title').value;
-    const description = taskEditor.querySelector('input#description').value;
+    const title = taskEditor.querySelector('#title').value;
+    const description = taskEditor.querySelector('#description').value;
     const action = taskEditor.getAttribute('data-action');
     if (action === 'create') {
         const bucketId = parseInt(taskEditor.getAttribute('data-bucket'));
@@ -51,8 +51,8 @@ async function saveTask() {
 function closeTaskEditor() {
     const taskEditor = document.getElementById('task_editor');
     taskEditor.style.display = 'none';
-    taskEditor.querySelector('input#title').value = '';
-    taskEditor.querySelector('input#description').value = '';
+    taskEditor.querySelector('#title').value = '';
+    taskEditor.querySelector('#description').value = '';
     taskEditor.removeAttribute('data-action');
     taskEditor.removeAttribute('data-task');
     taskEditor.removeAttribute('data-bucket');
